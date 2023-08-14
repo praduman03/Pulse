e2870bd71d1adedba2656d8b98647021e7e3ca2f
Praduman <praduman.kachhwaha@kalvium.community>
2023-08-14 20:28:23 +0530

    created user signup route

---
+++ nodejs/routes/userRoutes.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/userModel");
+
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!email || !name || !password) {
+    return res.status(422).json({ error: "please fill all the fields" });
+  }
+  User.findOne({ $or: [{ email: email }, { name: name }] })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exist with this email" });
+      }
+
+      //   User.findOne({ name: name }).then((savedUser) => {
+      //     if (savedUser) {
+      //       return res.status(422).json({ error: "name already exist" });
+      //     }
+      //   });
+
+      bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          email,
+          password: hashedPassword,
+          name,
+        });
+
+        user.save().then((message) => {
+          return res.status(201).json({ message: "user created successfully" });
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
--- nodejs/server.js
+++ nodejs/server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+mongoose.set("strictQuery", false);
+
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 require("dotenv").config();
 
 // middlewares
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use("/user", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
